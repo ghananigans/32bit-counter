59711f4585c75e154d1eee125de13d58081e3b1f
Ghanan Gowripalan <ghanan94@gmail.com>
2016-08-22 23:01:10 -0400

    Simple 32bit counter

--- count.v
+++ count.v
@@ -1,1 +1,51 @@
+/*
+ * MODULE: count
+ *
+ * DESCRIPTION: 32-bit counter with an active-low synchronous reset.
+ *
+ * PARAMETERS:
+ *   N/A
+ *
+ * INPUTS:
+ *   clk
+ *     - Clock.
+ *   rstn
+ *     - Active-low reset.
+ *
+ * OUTPUTS:
+ *   cnt [31:0]
+ *     - Counter output.
+ */
+module count (
+	clk,
+	rstn,
+	cnt
+);
 
+	// Input Port Declarations
+	input			clk;
+	input			rstn;
+
+	// Output Port Declarations
+	output	[31:0]	cnt;
+
+	// Regs
+	reg		[31:0]	counter;
+
+	// Counter
+	always @ ( posedge clk )
+	begin : COUNTER
+		if ( rstn == 0 )
+		begin
+			counter <= 32'd0;
+		end
+		else
+		begin
+			counter <= counter + 1;
+		end
+	end
+
+	// Assign outputs
+	assign cnt = counter;
+
+endmodule
--- count_tb.v
+++ count_tb.v
@@ -1,1 +1,42 @@
+/*
+ * MODULE: count_tb
+ *
+ * DESCRIPTION: Testbench for 32-bit counter with an active-low synchronous
+ *              reset.
+ *
+ * PARAMETERS:
+ *   N/A
+ *
+ * INPUTS:
+ *   N/A
+ *
+ * OUTPUTS:
+ *   N/A
+ */
+module count_tb ();
+	// Internal
+	reg				clk;
+	reg				rstn;
+	wire	[31:0]	cnt;
 
+	count COUNTER (
+		.clk( clk ),
+		.rstn( rstn ),
+		.cnt( cnt )
+	);
+
+	// Generate a clock
+	initial
+	begin
+		clk = 0;
+		forever #5 clk = ~clk;
+	end
+
+	// Reset
+	initial
+	begin
+		rstn = 0;
+		#10 rstn = 1;
+	end
+
+endmodule
