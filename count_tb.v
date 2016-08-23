948a793b30b9f655e51eb29ad191bfbda162ab2f
Ghanan Gowripalan <ghanan94@gmail.com>
2016-08-22 23:06:33 -0400

    leave reset active for > 1 period

--- count_tb.v
+++ count_tb.v
@@ -38,9 +38,15 @@ module count_tb ();
 		rstn = 0;
 		#10 rstn = 1;
 
-		// cnt sound be 10. Reset now.
+		// cnt should be 10. Reset now.
 		#100 rstn = 0;
 		#10 rstn = 1;
+
+		// cnt should be 5. Reset now.
+		#50 rstn = 0;
+
+		// Leave it at 0 for 5 periods.
+		#50 rstn = 1;
 	end
 
 endmodule
